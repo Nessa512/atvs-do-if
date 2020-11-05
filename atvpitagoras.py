889dd8b6dea3375f32e10737502a3eea9a78ca1d
Sony <74019376+Sonyemin@users.noreply.github.com>
2020-11-05 18:12:41 -0300

    atvpitagoras.py
    Atividade 1 da matéria de Informática Básica, ministrada pelo prof. Davis Vasconcelos, do curso de Tec. Em Telecom

---
+++ atvpitagoras.py
@@ -0,0 +1,12 @@
+print("INSTITUTO FEDERAL DO CEARÁ - CAMPUS FORTALEZA")
+print("Aluna Soniely Soares Silva")
+print("Curso de info. Básica, Prof. Davis Vasconcelos")
+print()
+print("• Atividade:")
+C1 = int ( input ("digite o valor do cateto 1 ") )
+C2 = int ( input ("digite o valor do cateto 2 ") )
+SDQDC = C1 **2 + C2 **2
+print("Soma dos quadrados dos catetos =", SDQDC )
+import math
+H = math.sqrt(SDQDC)
+print("Hipotenusa =", H)
